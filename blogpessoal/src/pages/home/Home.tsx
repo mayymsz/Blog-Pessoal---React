6a878f7790eb58b00c994a88fc296f837b6c8f6d
mayymsz <maahmoura2003@gmail.com>
2025-09-03 09:33:19 -0300

    nova postagem

--- blogpessoal/src/pages/home/Home.tsx
+++ blogpessoal/src/pages/home/Home.tsx
@@ -1,38 +1,21 @@
-function Home() {
-  return (
-    <>
-    <div style={{
-        width:"100vw",
-        display:"flex",
-        justifyContent:"center"
-    }}>
-        <div>
-            <div style = {{
-                width:"80vw",
-                display:"flex",
-                flexDirection:"column",
-                alignItems:"center"
-            }}>
-                <h2>Seja Bem vindo!</h2>
-                <p>Expresse aqui seus pensamentos e opiniões</p>
-
-            </div>
-            <div style = {{
-                width:"80vw",
-                display:"flex",
-                flexDirection:"column",
-                alignItems:"center"
-            }}>
-                <img
-                src="https://i.imgur.com/VpwApCU.png"
-                alt="Imagem da página Home"
-                width="400px"
-                />
+<div className="bg-indigo-900 flex justify-center">
+    <div className="container grid grid-cols-2 text-white">
+        <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className='text-5x1 font-bold'>
+                Sejam bem vindos!!!
+            </h2>
+            <p className='text-x1'>
+                Explore aqui seus pensamentos e opiniões
+            </p>
+            <div className="flex justify-around gap-4">
+                <div className='rounded text-white'
+                    border-white border-solid border-2 py-2 px-4>
+                    Nova Postagem
+                </div>
             </div>
         </div>
+        <div className="flex justify-center">
+            <img src="https://i.imgur.com/fyfri1v.png"alt="Imagem Página Home"className='w-2/3'></img>
+        </div>
     </div>
-    </>
-    )
-}
-
-export default Home
+</div>
